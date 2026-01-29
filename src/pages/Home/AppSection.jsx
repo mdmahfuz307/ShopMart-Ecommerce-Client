463adc6a5520c7dcd1cafaee9d2d7214cf9afec8
mdmahfuz307 <njnirob2019@gmail.com>
2026-01-30 04:11:40 +0600

    App section Done

--- src/pages/Home/AppSection.jsx
+++ src/pages/Home/AppSection.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
+const btnText = "Sign Up for Free";
+const title = "Shop Anytime, Anywhere!";
+const desc = "Take shop on your any device with our app & learn all time what you want. Just download & install & start to learn.";
 
 const AppSection = () => {
   return (
-    <div>AppSection</div>
-  )
+    <div className="app-section padding-tb">
+      <div className="container">
+        <div className="section-header text-center">
+          <Link to="/sign-up" className="lab-btn mb-4">
+            {btnText}
+          </Link>
+          <h2 className="title">{title}</h2>
+          <p>{desc}</p>
+        </div>
+
+        <div className="section-wrapper">
+          <ul className="lab-ul">
+            <li>
+              <a href="#">
+                <img src="/src/assets/images/app/01.jpg" alt="app" />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="/src/assets/images/app/02.jpg" alt="app" />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default AppSection
+export default AppSection;
