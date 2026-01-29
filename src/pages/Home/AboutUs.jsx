b43365918a3ef5e1e14a2b34f2dd564881337d17
mdmahfuz307 <njnirob2019@gmail.com>
2026-01-30 04:01:07 +0600

    AboutUs Page

--- src/pages/Home/AboutUs.jsx
+++ src/pages/Home/AboutUs.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import CountUp from "react-countup";
+import { Link } from "react-router-dom";
+
+
 const subTitle = "Why Choose Us";
 const title = "Become a Marchant";
 const desc =
@@ -25,7 +29,50 @@ const countList = [
 ];
 
 const AboutUs = () => {
-  return <div>AboutUs</div>;
+  return <div className='instructor-section style-2 padding-tb section-bg-ash'>
+    <div className='container'>
+        <div className='section-wrapper'>
+            <div className='row g-4 justify-content-center align-items-center row-cols-xl-3 row-cols-md-2 row-cols-1'>
+                <div className='col'>
+                    {
+                        countList.map((val, i) => (
+                            <div key={i} className='count-item'>
+                                <div className='count-inner'>
+                                    <div className='count-icon'>
+                                        <i className={val.iconName}></i>
+                                    </div>
+                                    <div className="count-content">
+                                        <h2>
+                                        <span className='count'><CountUp end={val.count} /></span>
+                                        <span>+</span>
+                                        </h2>
+                                        <p>{val.text}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+
+                <div className='col'>
+                    <div className='instructor-content'>
+                        <p className="subtitle">{subTitle}</p>
+                        <h2 className="title">{title}</h2>
+                        <p>{desc}</p>
+                        <Link to="/sign-up" className="lab-btn">{btnText}</Link>
+                    </div>
+
+                </div>
+
+                <div className="col">
+                    <div className="instructor-thumb">
+                        <img src="/src/assets/images/instructor/01.png" alt="instructor" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>;
 };
 
 export default AboutUs;
